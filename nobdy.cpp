a326614b25c06786a19bde04327ebb13ab85c65b
7hangtime <dthrowe1@charlotte.edu>
2025-09-09 22:16:34 -0400

    randomConfig finished

--- nobdy.cpp
+++ nobdy.cpp
@@ -16,11 +16,25 @@ struct sim{
         double minMass = 1.0, maxMass = 10.0;
         double minPosition = -100.0, maxPosition = 100.0;
         double minVelocity = -10.0, maxVelocity = 10.0;
+        double minForce = -1.0, maxForce = 1.0;
         std::srand(std::time(0));
-        
+
         int numParticles = minParticles + (std::rand() % (maxParticles - minParticles + 1));
 
+        // Resizing is more efficient than pushing back elements one by one
+        masses.resize(numParticles);
+        positions.resize(numParticles, std::vector<double>(3));
+        velocities.resize(numParticles, std::vector<double>(3));
+        forces.resize(numParticles, std::vector<double>(3, 0.0));
 
+        for (int i = 0; i < numParticles; i++) {
+            masses[i] = minMass + static_cast<double>(std::rand()) / RAND_MAX * (maxMass - minMass);
+            for (int j = 0; j < 3; ++j) {
+                positions[i][j] = minPosition + static_cast<double>(std::rand()) / RAND_MAX * (maxPosition - minPosition);
+                velocities[i][j] = minVelocity + static_cast<double>(std::rand()) / RAND_MAX * (maxVelocity - minVelocity);
+                forces[i][j] = minForce + static_cast<double>(std::rand()) / RAND_MAX * (maxForce - minForce);
+            }
+        }
 
     }
 
