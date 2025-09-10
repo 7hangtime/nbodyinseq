6b45f4f3e0afd0c9e05bfd65e39ebdfe65529b41
7hangtime <dthrowe1@charlotte.edu>
2025-09-09 22:32:51 -0400

    Finished predefined config

--- nobdy.cpp
+++ nobdy.cpp
@@ -40,8 +40,33 @@ struct sim{
     }
 
     void predefinedConfig() {
-        
+        // Using Sun, Moon, and Erth like in pdf instruction
+        // Did not take physics so most of these nums will just be rando except for mass
+        masses.resize(3);
+        positions.resize(3, std::vector<double>(3));
+        velocities.resize(3, std::vector<double>(3));
+        forces.resize(3, std::vector<double>(3));
 
+        // Particle 1 (sun)
+        // Sun is the center so setting everything to 0
+        masses[0] = 1.989e30; // mass of the sun
+        positions[0] = {0.0, 0.0, 0.0}; 
+        velocities[0] = {0.0, 0.0, 0.0}; 
+        forces[0] = {0.0, 0.0, 0.0}; 
+
+        // Particle 2 (moon)
+        // literally typing random numbers except for mass from Google
+        masses[1] = 7.342e22; // mass of the moon
+        positions[1] = {10.0, 5.0, 2.0}; 
+        velocities[1] = {0.0, 500.0, 100.0}; 
+        forces[1] = {0.0, 0.0, 0.0};
+
+
+        // Particle 3 (earth)
+        masses[2] = 5.972e24; // mass of the earth
+        positions[2] = {20.0, 10.0, 5.0}; 
+        velocities[2] = {0.0, 300.0, 50.0}; 
+        forces[2] = {0.0, 0.0, 0.0};
     }
 
     void loadFromFile(const std::string& filename) {
